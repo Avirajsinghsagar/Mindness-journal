528af6bcf9f8c1582b212ee853828e66a3f99eee
Avirajsinghsagar <avirajsinghsagar@gmail.com>
2026-06-21 20:39:38 +0530

    Update API URL to Render backend

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -4,8 +4,13 @@ const API = axios.create({
   baseURL: "http://localhost:5000/api",
 });
 
+const API = axios.create({
+  baseURL: process.env.REACT_APP_API_URL || "https://mindwell-backend-76fd.onrender.com/api",
+});
+
 API.interceptors.request.use((req) => {
   const token = localStorage.getItem("token");
+  
 
   if (token) {
     req.headers.Authorization = `Bearer ${token}`;
